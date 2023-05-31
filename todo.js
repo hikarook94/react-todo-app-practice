6703f11a5dba23e7277618a3e1e85846d167459e
hikarook94 <walrus4l145@icloud.com>
2023-05-31 20:27:06 +0900

    セットアップ

--- todo.js
+++ todo.js
@@ -1,1 +1,24 @@
 // Write your code
+class Board extends React.Component {
+  constructor(props) {
+      console.log('test');
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello, World!</h1>
+    )
+  }
+}
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {}
+}
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(<Board />);
